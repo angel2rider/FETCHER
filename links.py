5b7b68db6c4bbe27dce77d6f3e3a27ee68737234
TheGT <159677645+angel2rider@users.noreply.github.com>
2025-09-30 11:51:44 +0530

    Create links.py

---
+++ links.py
@@ -0,0 +1,27 @@
+# links.py
+
+# Format: "appname": [{"platform": "win/mac/linux/android/ios", "version": "x.y", "url": "direct link"}]
+APPS = {
+    "gtasanandreas": [
+        {"platform": "mac", "version": "1.01", "url": "https://github.com/angel2rider/FETCHER/releases/download/v1.0/gtasanandreas-1.01-mac.dmg"}
+    ],
+    "capcut": [
+        {"platform": "android", "version": "14.6.0", "url": "https://github.com/angel2rider/FETCHER/releases/download/v1.0/capcut-14.6.0-android.apk"},
+        {"platform": "mac", "version": "5.7.1", "url": "https://github.com/angel2rider/FETCHER/releases/download/v1.0/capcut-5.7.1-mac.dmg"},
+        {"platform": "win", "version": "7.2.0", "url": "https://github.com/angel2rider/FETCHER/releases/download/v1.0/capcut-7.2.0-win.exe"}
+    ],
+    "premierpro": [
+        {"platform": "mac", "version": "25.5.0", "url": "https://download.akirabox.com/uploads/users/dkD3YV1JzLQK/cobIaSURMG3qtmBFOde-Premiere%2520Pro%252025.5%2520%255BMonter%2520Group%255D.dmg?access=wMhRDZxtWeyJXYuR2M4IWcw4Wd0wWNnZjanhWb&token=FBMXBg0EHAQWDl9ZUU9bWlRMHwoHTwoZAw"}
+    ],
+    "crossover": [
+        {"platform": "mac", "version": "25.1.1", "url": "https://github.com/angel2rider/FETCHER/releases/download/v1.0/crossover-25.1.1-mac.dmg"}
+    ],
+    "parallels": [
+        {"platform": "mac", "version": "26.1.0", "url": "https://github.com/angel2rider/FETCHER/releases/download/v1.0/parallels-26.1.0-mac.dmg"}
+    ],
+    "davinciresolve": [
+        {"platform": "mac", "version": "20.2.1", "url": "https://download.akirabox.com/uploads/users/dkD3YV1JzLQK/couorx4LpjDdYQ1l9de-DaVinci%2520Resolve%2520Studio%2520v20.2.1%2520macOS.dmg?access=QchNDZntGMyRTYoR2awQHN0N2d11WNvZzYndXb&token=AAAEAgoCBwYBEh0HEgRDRlRPQEVVTAMVB08WBgM"}
+    ]
+}
+
+ICON_BASE_URL = "https://raw.githubusercontent.com/angel2rider/FETCHER/main/icons/"
